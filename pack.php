55918848dc61b0e6e5c1669b30e3d9396debcde6
Holson Mpangala <holsonm74@gmail.com>
2022-02-09 12:19:57 +0100

    Package PHP

---
+++ pack.php
@@ -0,0 +1,125 @@
+    <div class="list-info">
+        <i class="icon-cpu"></i> 
+        <span class="spec">CPU</span><br> 
+        <span>4x 3.20Ghz 2 Cores</span>
+    </div>     
+    <div class="list-info">
+        <i class="icon-ram"></i> 
+        <span class="spec">RAM</span><br> 
+        <span>32GB RAM</span>
+    </div>     
+    <div class="list-info">
+        <i class="icon-drivessd"></i> 
+        <span class="spec">SSD Disque</span><br> 
+        <span>100 Go Espace disque SSD</span>
+    </div>     
+    <div class="list-info">
+        <i class="icon-git"></i> 
+        <span class="spec">Bande Passante</span><br> 
+        <span>10 Go Bande passante</span>
+    </div>  
+    <div class="list-info">
+        <i class="icon-domains"></i> 
+        <span class="spec">Domaines</span><br> 
+        <span>5 Domains TLD</span>
+    </div>   
+    <div class="list-info">
+        <i class="icon-emailopen"></i> 
+        <span class="spec">Email</span><br> 
+        <span>50  Adresses Mails pro</span>
+    </div>   
+    <div class="list-info">
+        <i class="icon-drives"></i> 
+        <span class="spec">Ftp</span><br> 
+        <span>10 Comptes FTP</span>
+    </div>   
+
+
+
+SMART
+
+    <div class="list-info">
+        <i class="icon-cpu"></i> 
+        <span class="spec">CPU</span><br> 
+        <span>4x 3.20Ghz 2 Cores</span>
+    </div>     
+    <div class="list-info">
+        <i class="icon-ram"></i> 
+        <span class="spec">RAM</span><br> 
+        <span>32GB RAM</span>
+    </div>     
+    <div class="list-info">
+        <i class="icon-drivessd"></i> 
+        <span class="spec">SSD Disque</span><br> 
+        <span>100 Go Espace disque SSD</span>
+    </div>     
+    <div class="list-info">
+        <i class="icon-git"></i> 
+        <span class="spec">Bande Passante</span><br> 
+        <span>100 Go Bande passante</span>
+    </div>  
+    <div class="list-info">
+        <i class="icon-domains"></i> 
+        <span class="spec">Domaines</span><br> 
+        <span>10 Domains TLD</span>
+    </div>   
+    <div class="list-info">
+        <i class="icon-emailopen"></i> 
+        <span class="spec">Email</span><br> 
+        <span>100  Adresses Mails pro</span>
+    </div>   
+    <div class="list-info">
+        <i class="icon-drives"></i> 
+        <span class="spec">Ftp</span><br> 
+        <span>20 Comptes FTP</span>
+    </div>   
+
+
+    <ul class="specs-content " >
+        <li><i class="icon-cpu"></i> 4x 3.20Ghz 2 Cores</li>
+        <li><i class="icon-ram"></i> 32GB RAM </li>
+        <li><i class="icon-drivessd"></i> ILLIMITÉ Espace disque SSD</li>
+        <li><i class="icon-git"></i> Bande passante illimitée</li>
+<li><i class="icon-domains"></i> 20 Domains TLD</li>
+<li><i class="icon-emailopen"></i> 100  Adresses Mails pro</li>
+<li><i class="icon-drives"></i> 20 Comptes FTP</li>
+    </ul>
+
+
+Master
+
+    <div class="list-info">
+        <i class="icon-cpu"></i> 
+        <span class="spec">CPU</span><br> 
+        <span>4x 3.20Ghz 2 Cores</span>
+    </div>     
+    <div class="list-info">
+        <i class="icon-ram"></i> 
+        <span class="spec">RAM</span><br> 
+        <span>32GB RAM</span>
+    </div>     
+    <div class="list-info">
+        <i class="icon-drivessd"></i> 
+        <span class="spec">SSD Disque</span><br> 
+        <span>Espace disque SSD Illimité</span>
+    </div>     
+    <div class="list-info">
+        <i class="icon-git"></i> 
+        <span class="spec">Bande Passante</span><br> 
+        <span>Bande passante illimitée</span>
+    </div>  
+    <div class="list-info">
+        <i class="icon-domains"></i> 
+        <span class="spec">Domaines</span><br> 
+        <span>Domains TLD Illimité</span>
+    </div>   
+    <div class="list-info">
+        <i class="icon-emailopen"></i> 
+        <span class="spec">Email</span><br> 
+        <span>Adresses Mails pro Illimité</span>
+    </div>   
+    <div class="list-info">
+        <i class="icon-drives"></i> 
+        <span class="spec">Ftp</span><br> 
+        <span>Comptes FTP Illimité</span>
+    </div>   
